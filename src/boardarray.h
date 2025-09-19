2e859e11ea7724558054909af7899f642983d671
wengGitH <wengie.arriola@cit.edu>
2025-09-20 00:14:49 +0800

    feat: implement add() method for boardarray

--- src/boardarray.h
+++ src/boardarray.h
@@ -13,10 +13,55 @@ class BoardArray : public Board {
             index = 0;
         }
 
-        void add(Entry* entry) {
+        void add(Entry* entry) 
+        {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-            return;
+            if(index == 0)
+            {
+                array[0] = *entry;
+                index++;
+                return;
+            }
+
+            int pos = 0;
+            for(int i = 0; i < index; i++)
+            {
+                if(entry->score > array[i].score)
+                {
+                    pos = i;
+                    break;
+                }
+                pos++;
+            }
+
+            if(index < SIZE)
+            {
+                for(int i = index; i > pos; i--)
+                {
+                    array[i] = array[i-1];
+                }
+                array[pos] = *entry;
+                index++;
+            }
+            else if(pos < SIZE)
+            {
+                if(entry->score <= array[SIZE-1].score)
+                {
+                    cout<<entry->score<<"'s score is too low to be added!'"<<endl;
+                    return;
+                }
+                for(int i = SIZE - 1; i > pos; i--)
+                {
+                    array[i] = array[i-1];
+                }
+                array[pos] = *entry;
+            }
+            else
+            {
+                cout<<entry->name<<"'s score is too low to be added!"<<endl;
+            }
+
         }
 
         void print() {
