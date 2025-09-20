a7aab320597619097433b47375fc911f7546988f
Precious Dianne Sudueste <diannewhales@gmail.com>
2025-09-20 13:16:10 +0800

    updated add () function in boardarray

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,6 +16,19 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+
+            for (int i = 0; i < SIZE; i++){
+                if (entry->compare(&array[i])){
+                    for(int j = SIZE - 1; j > i; j--){
+                        array[j] = array[j - 1];
+                        }
+                    array[i] = *entry;
+                    index++;
+                    return;
+                }
+            }
+
+            cout << entry->name << "'s score is too low to be added!" << endl;
             
         }
 
